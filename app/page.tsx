441920077c1f20f8a861e1c7bba4ff2c743cfb2e
tabularization <127825421+tabularization@users.noreply.github.com>
2025-04-21 01:14:41 -0400

    ui tweak 2

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="relative flex flex-col text-[#2E2E2E] h-full mt-30 items-center justify-start font-sans tracking-[-2px]">
       <div className="fixed inset-0 bg-[url('/bg5.png')] bg-cover bg-top-left blur-[4px] z-[-1] animate-bg-zoom"></div>
-      <h1 className="text-[3.5rem] font-[500] capitalize">Discover <span className="italic tracking-[-2px]">legendary</span> cards, Start Discovering</h1>
+      <h1 className="text-[3.5rem] font-[500] capitalize">Discover <span className="italic tracking-[-2px]">legendary</span> cards, Start Collecting</h1>
       <h2 className="text-[3rem] font-sans font-[300]">Over <span className="font-mono underline">1000+</span> Cards From Different Sets</h2>
       <Link href="/sets">
         <button className="border cursor-pointer rounded-[60] text-[1.2rem] mt-10 py-3 px-10 tracking-[1.5px] font-[500] uppercase text-gray-900">
           Open pack
         </button>
       </Link>
-      <Image src="/img1.png" alt="Picture drawing of 2 people playing cards." height={400} width={600} />
+      <Image src="/img1.png" alt="Picture drawing of 2 people playing cards." draggable={false} height={400} width={600} />
     </div>
   );
 }
