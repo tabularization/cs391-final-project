3a79ef574dc56d69d1eacd9c4c0cc6a2cfb5473c
Alvin Zhu <123048238+alvinz23@users.noreply.github.com>
2025-04-28 00:01:42 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+//Layout page, renders navigation bar with all our components/page
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
